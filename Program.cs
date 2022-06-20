6b4282dd6f0a393ea136fbd58a9cdf2413b797b1
evgeniy kliukvin <evg.klukvin@mail.ru>
2022-06-21 00:34:12 +0700

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1. Пользователь вводит число N (N > 0). Программа должна вывести N единиц на экран.
+// N = 4 -> 1, 1, 1, 1
+// N = 2 -> 1, 1
+// Console.WriteLine("введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int d = 0;
+
+// while (d<a)
+// {
+//     Console.WriteLine("1");
+//      d++ ;
+// }
+
+// Задача 2. Даны два числа a, b, такие что a < b.
+// Вывести на экран сколько раз число a поместиться в числе b.
+
+// Console.WriteLine("введите число a");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число b");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int c = 0;
+
+// c=b/a;
+// Console.WriteLine(c);
+
+// Задача 3. На ввод подаётся рост трёх спортсменов.
+// Расположить их от большего к меньшему.
+
